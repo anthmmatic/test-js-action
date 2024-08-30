bd01e9e202aabb1a46ac01fa818d0cb2e0c8741b
Anthony <86444119+anthms@users.noreply.github.com>
2024-08-30 16:21:22 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
+import * as core from '@actions/core'
+import * as cache from '@actions/cache'
+
 async function run(): Promise<void> {
-  const core = require('@actions/core')
-  const cache = require('@actions/cache')
   try {
     const paths = [ 'node_modules' ]
     const key = core.getInput('cache-key')
